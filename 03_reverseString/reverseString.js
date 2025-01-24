c8c59224e528bb2560fbe73357d6122a37eb1057
iren42 <isabelleren618@gmail.com>
2025-01-24 19:39:13 +0100

    feat: implement reverseString()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	let i = 0;
+	let arrayStr = Array.from(str);
+	let buffer = "";
 
+	while (i < str.length / 2)
+	{
+		buffer = arrayStr[i]
+		arrayStr[i] = arrayStr[str.length - 1 - i];
+		arrayStr[str.length - 1 - i] = buffer;
+		i++;
+	}
+	return (arrayStr.join(""));
 };
 
 // Do not edit below this line
