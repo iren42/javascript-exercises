ac3284321e17c6e21e4018fdd70d0e24519c26b9
iren42 <isabelleren618@gmail.com>
2025-01-24 20:36:25 +0100

    feat: all passes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+/* x °F ≘ (x − 32) × 5/9 °C  */
+const convertToCelsius = function(fahr) {
+	let res = (fahr - 32) * (5 / 9);
+	if (Number.isInteger(res))
+		return (res);
+	return (Number(res.toFixed(1)));
+
 };
 
-const convertToFahrenheit = function() {
+/* x °C ≘ (x × 9/5 + 32) °F */
+const convertToFahrenheit = function(celcius) {
+	let res = (celcius * (9 / 5) + 32);
+	if (Number.isInteger(res))
+		return (res);
+	return (Number(res.toFixed(1)));
 };
 
 // Do not edit below this line
