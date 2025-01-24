ab434aa5a17451f6036509b9ddb31de358e3c3b9
iren42 <isabelleren618@gmail.com>
2025-01-24 20:17:07 +0100

    feat: all passes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(posIntA, posIntB) {
+	if (posIntA < 0 || posIntB < 0 ||
+		!Number.isInteger(posIntA) || !Number.isInteger(posIntB))
+		return ("ERROR");
+	let	sum = 0;
+	let	i = posIntA;
+	if (posIntA > posIntB)
+		i = posIntB;
+	while (i <= posIntB || i <= posIntA)
+	{
+		sum += i++;
+	}
+	return (sum);
 };
 
 // Do not edit below this line
