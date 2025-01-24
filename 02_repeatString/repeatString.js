7c0fbf63ee2190abba8ab88c29ea656bdab04385
iren42 <isabelleren618@gmail.com>
2025-01-24 17:19:08 +0100

    feat: all passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+	if (num < 0)
+		return ("ERROR");
 
+	let res = "";
+	let i = 0;
+	while (i++ < num)
+	{
+		res += str;
+	}
+	return (res);
 };
 
 // Do not edit below this line
