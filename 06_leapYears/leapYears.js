a4a188b2a9f035636fdda32e87e76b089c43fe5e
iren42 <isabelleren618@gmail.com>
2025-01-24 20:23:30 +0100

    feat: all passes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+/* 
+ * Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+ * */
+const leapYears = function(yearToDetermine) {
+	if (yearToDetermine % 100 == 0 && yearToDetermine % 400 != 0)
+		return (false);
+	if (yearToDetermine % 4 == 0)
+		return (true);
+	return (false);
 };
 
 // Do not edit below this line
