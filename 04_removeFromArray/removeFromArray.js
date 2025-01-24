8e3f9875fac0385c4cac16d58d7375803b577409
iren42 <isabelleren618@gmail.com>
2025-01-24 20:02:28 +0100

    feat: all passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nbToRemove) {
+	for (const nb of nbToRemove)
+	{
+		while (array.includes(nb))
+			array.splice(array.indexOf(nb), 1);
+	}
+	return (array);
 };
 
 // Do not edit below this line
