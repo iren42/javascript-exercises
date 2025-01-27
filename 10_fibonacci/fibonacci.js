eab1591ded0ebcfc91aeee58d8b6af5c24116a08
iren42 <isabelleren618@gmail.com>
2025-01-27 21:00:37 +0100

    feat: implement fibonacci()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+/* A series of numbers in which each number ( Fibonacci number ) is the sum of
+ * the two preceding numbers. In this exercise, the Fibonacci sequence used is
+ * 1, 1, 2, 3, 5, 8, etc. 
+ */
 
+const fibonacci = function(index) {
+	if (index < 0)
+		return ("OOPS");
+	if (index == 0)
+		return (0);
+	if (index == 1 || index == 2)
+		return (1);
+	let	firstPrecedent = fibonacci(index - 2);
+	let	secondPrecedent = fibonacci(index - 1);
+	return (firstPrecedent + secondPrecedent);
 };
 
 // Do not edit below this line
