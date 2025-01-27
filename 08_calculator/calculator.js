8f7fd9ec50e57f6814718518c622d49b37303659
iren42 <isabelleren618@gmail.com>
2025-01-27 19:47:56 +0100

    feat: all passes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(a, b) {
+	return (a + b);	
+};
+
+const subtract = function(a, b) {
+	return (a - b);	
+};
+
+const sum = function(arr) {
+	return (arr.reduce( (total, a) => {
+		return (total + a);
+	}, 0));
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+	return (arr.reduce( (total, a) => {
+		return (total * a);
+	}, 1));
 };
 
-const sum = function() {
-	
+const power = function(base, exponent) {
+	if (exponent == 1)
+		return (base);
+	return (base * power(base, exponent - 1));
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(pos) {
+	if (pos === 0)
+		return (1);
+	return (pos * factorial(pos - 1));
 };
 
 // Do not edit below this line
